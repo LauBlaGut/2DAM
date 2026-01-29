9274041dc34b1360680c49fa8d302ed2f98e92de
Laura Blanco <lblancogutierrez@safareyes.es>
2026-01-29 20:19:00 +0100

    añadiendo crossorigin

--- Acceso a Datos/safaeventosbbdd/src/main/java/com/safa/safaeventosbbdd/config/WebConfig.java
+++ Acceso a Datos/safaeventosbbdd/src/main/java/com/safa/safaeventosbbdd/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "https://localhost",
                         "https://safaeventos-angular.onrender.com",
                         "http://localhost:8100",
                         "http://localhost:4200"
